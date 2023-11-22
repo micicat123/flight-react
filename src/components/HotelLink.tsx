448d8ab2280562663d87ac2c28ddcebf1b928b40
Marcel Zep <76478960+micicat123@users.noreply.github.com>
2023-11-22 21:02:14 +0100

    Update HotelLink.tsx

--- src/components/HotelLink.tsx
+++ src/components/HotelLink.tsx
@@ -14,8 +14,8 @@ const HotelLink = (props:any) => { //props.competitionId
                 url: 'https://best-booking-com-hotel.p.rapidapi.com/booking/best-accommodation',
                 method: 'GET',
                 headers: {
-                  'X-RapidAPI-Key': '9b381a0de8mshc7bf7ed5988c10dp110c7djsn01898af74e2a',
-                  'X-RapidAPI-Host': 'best-booking-com-hotel.p.rapidapi.com'
+                  'X-RapidAPI-Key': 'api_key',
+                  'X-RapidAPI-Host': 'api_host'
                 },
                 params: {
                   'cityName': props.city,
